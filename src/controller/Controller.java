05fa21109e42ea9a5b59f6c5c5e30a3cd51f3f89
SenWangHost <senwang1992010@gmail.com>
2018-04-11 20:48:05 -0400

    add java jee6 library to the project and start a login page

---
+++ src/controller/Controller.java
@@ -0,0 +1,87 @@
+package controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import model.Model;
+/**
+ * Controller for actions.
+ * @author Team 8
+ *
+ */
+public class Controller extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+    Model model;
+
+    @Override
+    public void init() throws ServletException {
+        model = new Model(getServletConfig());
+
+		/* Actions here */
+
+    }
+
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        if (model.getRequireSSL() && !request.isSecure()) {
+            // If we're required to use SSL and we're not on a secure connection, redirect
+            String host = request.getServerName();
+            String port = (request.getServerPort() == 80) ? "" : ":8443";
+            response.sendRedirect("https://" + host + port + request.getRequestURI());
+            return;
+        }
+
+        String nextPage = performTheAction(request);
+        sendToNextPage(nextPage, request, response);
+    }
+
+    private String performTheAction(HttpServletRequest request) {
+        String servletPath = request.getServletPath();
+        String action = getActionName(servletPath);
+        System.out.println("ServletPath: "+servletPath);
+        System.out.println("[perform the action]Action: " + action);
+
+        return Action.perform(action, request);
+    }
+
+    private void sendToNextPage(String nextPage, HttpServletRequest request, HttpServletResponse response)
+            throws IOException, ServletException {
+
+        System.out.println(nextPage);
+        if (nextPage == null) {
+            response.sendError(HttpServletResponse.SC_NOT_FOUND, request.getServletPath());
+            return;
+        }
+
+        if (nextPage.endsWith(".do")) {
+            response.sendRedirect(nextPage);
+            return;
+        }
+
+        if (nextPage.endsWith(".jsp")) {
+            RequestDispatcher d = request.getRequestDispatcher("WEB-INF/view/" + nextPage);
+            d.forward(request, response);
+            return;
+        }
+
+        throw new ServletException(
+                Controller.class.getName() + ".sendToNextPage(\"" + nextPage + "\"): invalid extension.");
+    }
+
+    private String getActionName(String path) {
+        int slash = path.lastIndexOf('/');
+        return path.substring(slash + 1);
+    }
+}
---
+++ src/model/Model.java
@@ -0,0 +1,42 @@
+package model;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+
+import org.genericdao.ConnectionPool;
+import org.genericdao.DAOException;
+/**
+ * Model class.
+ * @author Team 8
+ *
+ */
+public class Model {
+
+
+    private boolean requireSSL;
+
+    /**
+     * Constructor.
+     * @param config
+     * @throws ServletException
+     */
+    public Model(ServletConfig config) throws ServletException {
+
+        requireSSL = new Boolean(config.getInitParameter("requireSSL"));
+        String jdbcDriver = config.getInitParameter("jdbcDriverName");
+        String jdbcURL = config.getInitParameter("jdbcURL");
+
+        try {
+            ConnectionPool pool = new ConnectionPool(jdbcDriver, jdbcURL);
+			/* DAOs */
+
+        } catch (DAOException e) {
+            throw new ServletException(e);
+        }
+    }
+
+    public boolean getRequireSSL() {
+        return requireSSL;
+    }
+
+}
